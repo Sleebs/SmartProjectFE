202aaaa88abfed244fc5b37766bfe5acf5dc6eb2
Dokke <40470116+Sleebs@users.noreply.github.com>
2023-06-21 16:44:24 +0200

    setup pinia store

--- SmartProject/src/main.ts
+++ SmartProject/src/main.ts
@@ -2,11 +2,14 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router/router";
+import { createPinia } from 'pinia'
 import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
 
 const app = createApp(App);
+const pinia = createPinia()
+
 
 app.component('VueDatePicker', VueDatePicker);
-
+app.use(pinia);
 app.use(router).mount("#app");
---
+++ SmartProject/src/stores/userStore.ts
@@ -0,0 +1,36 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useUserStore = defineStore("user", () => {
+  const user = ref<User | null>(null);
+
+  async function login(name: string, password: string): Promise<User> {
+    fetch("http://localhost:8080/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((resp) => resp.json)
+      .then((data: any) => {
+        const u: User = {
+          auth: data.auth,
+          name: data.name,
+          surname: data.surname,
+          site: data.site,
+        };
+        user.value = u;
+      })
+      .catch((err) => console.error(err));
+  }
+
+  return { user };
+});
+
+interface User {
+  auth: string;
+  name: string;
+  surname: string;
+  site: string;
+}
