e86af1825de1d562ca743498337cff897254afcc
marcostefani <m.stefani2023@gmail.com>
2023-06-28 16:18:59 +0200

    feat: fixed userStore

--- SmartProject/src/views/UserList.vue
+++ SmartProject/src/views/UserList.vue
@@ -1,69 +1,16 @@
 <script setup lang="ts">
 import ModalModify from "../components/ModalModify.vue";
 import ModalAdd from "../components/ModalAdd.vue";
+import { storeToRefs } from "pinia";
+import { useUserStore } from "../stores/userStore"
+
+const userStore = useUserStore()
+const { users } = storeToRefs(userStore)
+
+userStore.allUsers()
+
 let titolo = ["Nome", "Cognome", "Email", "Responsabile"];
-let listUsers = [
-  {
-    id: 1,
-    nome: "Andrea",
-    cognome: "Accolla",
-    email: "andrea.accolla@agmsolutions.net",
-    resp: "",
-    sede: "Milano",
-    tipoUser: "Post. fissa",
-    post: "A1",
-  },
-  {
-    id: 2,
-    nome: "Simone",
-    cognome: "Albadoro",
-    email: "simone.albadoro@agmsolutions.net",
-    resp: "Rossi",
-    sede: "Torino",
-    tipoUser: "Post. var.",
-    post: "--",
-  },
-  {
-    id: 3,
-    nome: "Marco",
-    cognome: "Amedura",
-    email: "marco.amedura@agmsolutions.net",
-    resp: "Rossi",
-    sede: "Torino",
-    tipoUser: "Admin",
-    post: "A2",
-  },
-  {
-    id: 4,
-    nome: "Francesco",
-    cognome: "Antoniozzi",
-    email: "francesco.antoniozzi@agmsolutions.net",
-    resp: "Barbagallo",
-    sede: "Milano",
-    tipoUser: "Coll. est.",
-    post: "A3",
-  },
-  {
-    id: 5,
-    nome: "Furio",
-    cognome: "Barbagallo",
-    email: "furio.barbagallo@agmsolutions.net",
-    resp: "",
-    sede: "Milano",
-    tipoUser: "Post. fissa",
-    post: "A4",
-  },
-  {
-    id: 6,
-    nome: "Andrea",
-    cognome: "Bellandi",
-    email: "andrea.bellandi.saladini@agmsolutions.net",
-    resp: "Barbagallo",
-    sede: "Milano",
-    tipoUser: "Post. var.",
-    post: "--",
-  },
-];
+let listUsers = [];
 </script>
 
 <template>
@@ -73,10 +20,7 @@ let listUsers = [
         <tr>
           <th>
             <button onclick="my_modal_3.showModal()">
-              <font-awesome-icon
-                :icon="['fas', 'circle-plus']"
-                style="font-size: large"
-              />
+              <font-awesome-icon :icon="['fas', 'circle-plus']" style="font-size: large" />
             </button>
           </th>
           <th>Nome</th>
@@ -92,18 +36,10 @@ let listUsers = [
         <tr v-for="list in listUsers" :key="list.id">
           <td class="text-primary">
             <div class="dropdown">
-              <label tabindex="0"
-                ><font-awesome-icon :icon="['fas', 'ellipsis-vertical']"
-              /></label>
-              <ul
-                tabindex="0"
-                class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-40 h-30"
-              >
+              <label tabindex="0"><font-awesome-icon :icon="['fas', 'ellipsis-vertical']" /></label>
+              <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-40 h-30">
                 <li>
-                  <button
-                    class="btn btn-link btn-xm flex content-center"
-                    onclick="my_modal_4.showModal()"
-                  >
+                  <button class="btn btn-link btn-xm flex content-center" onclick="my_modal_4.showModal()">
                     <font-awesome-icon :icon="['fas', 'pencil']" /> Modifica
                   </button>
                 </li>
