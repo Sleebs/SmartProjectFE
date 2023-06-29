4081b9eff47bc0e3fe4e5c65a39a86baddcf1518
Dokke <40470116+Sleebs@users.noreply.github.com>
2023-06-29 09:37:26 +0200

    feat: changes to Report  view

--- SmartProject/src/views/Report.vue
+++ SmartProject/src/views/Report.vue
@@ -2,6 +2,18 @@
 import Signup from "./Signup.vue";
 import Table from "../components/Table/Table.vue";
 import HTable from "../components/HoursTable/HTable.vue";
+import { onMounted } from "vue";
+
+import { useUserStore } from "../stores/userStore";
+import { storeToRefs } from "pinia";
+
+const userStore = useUserStore();
+
+const { users } = storeToRefs(userStore);
+
+onMounted(() => {
+  userStore.allUsers;
+});
 </script>
 
 <template>
